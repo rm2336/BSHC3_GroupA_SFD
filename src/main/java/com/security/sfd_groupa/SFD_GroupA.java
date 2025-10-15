d009d8b20e24690bddc0494858fe8e65cf3addae
rm2336 <x23361212@student.ncirl.ie>
2025-10-15 14:38:33 +0100

    First commit.

---
+++ src/main/java/com/security/sfd_groupa/SFD_GroupA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.security.sfd_groupa;
+
+/**
+ *
+ * @author rokom
+ * Security Fundamentals and Development, Group A
+ * Owner of Repository: Roko Matanovic
+ * Collaborators: Tom Brophy, Tom Byrne
+ */
+public class SFD_GroupA {
+
+    public static void main(String[] args) {
+        MainFrame mainMenu = new MainFrame();
+        mainMenu.setVisible(true);
+    }
+}
